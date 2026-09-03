fb8f1425aa3a819b99ac709ad8c04af67542484a
thejagithub <thejanaravindu2000@gmail.com>
2026-09-03 11:21:23 +0530

    Used import blocks to add existing resources to the terraform state

--- Lab-9-state-refactoring/main.tf
+++ Lab-9-state-refactoring/main.tf
@@ -1,3 +1,5 @@
+# removed all the resources from the main configuration to demonstrate state refactoring.
+
 # VPC
 # resource "aws_vpc" "main" {
 #   cidr_block = var.vpc_cidr
@@ -72,33 +74,108 @@
 # }
 
 # removed the route table resource to a new resource name to demonstrate state refactoring.
-removed {
-  from = aws_route_table.main
 
-  lifecycle {
-    destroy = false
+# removed all the resources from the main configuration to demonstrate state refactoring.
+
+# removed {
+#   from = aws_route_table.main
+
+#   lifecycle {
+#     destroy = false
+#   }
+# }
+
+# removed {
+#   from = aws_vpc.main
+#   lifecycle {
+#     destroy = false
+#   }
+# }
+
+# removed {
+#   from = aws_subnet.app
+
+#   lifecycle {
+#     destroy = false
+#   }
+# }
+
+# removed {
+#   from = aws_security_group.web-modified
+
+#   lifecycle {
+#     destroy = false
+#   }
+# }
+
+
+
+#VPC
+# imported via the cli using the command: terraform import aws_vpc.main "vpc-0aabb8db0d424419e"
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = "${var.prefix}-vpc"
+    Environment = var.environment
   }
 }
 
-removed {
-  from = aws_vpc.main
-  lifecycle {
-    destroy = false
+# Subnet
+# imported via the import block
+resource "aws_subnet" "app" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr
+
+  tags = {
+    Name        = "${var.prefix}-subnet"
+    Environment = var.environment
   }
 }
 
-removed {
-  from = aws_subnet.app
+import {
+    to = aws_subnet.app
+    id = "subnet-0869e378f642f96f0"
+}
 
-  lifecycle {
-    destroy = false
+import {
+    to = aws_security_group.web-modified
+    id = "sg-0b1a82fc8764b1333"
+}
+
+# this configuration is grabbed from the command 'terrafrom plan -generate-config-out=generated.txt'. The usage of this command is to generate a configuration file from the existing state. The generated configuration file can be used as a starting point for refactoring or modifying the infrastructure.
+
+resource "aws_security_group" "web-modified" {
+  description            = "Lab security group"
+  name                   = "${var.prefix}-web-sg"
+  region                 = "ap-northeast-1"
+  tags = {
+    Environment = "${var.environment}"
+    Name        = "${var.prefix}-web-sg"
+  }
+  tags_all = {
+    Environment = "${var.environment}"
+    Name        = "${var.prefix}-web-sg"
+  }
+  vpc_id = aws_vpc.main.id
+}
+
+
+# Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.prefix}-rt"
+    Environment = var.environment
   }
 }
 
-removed {
-  from = aws_security_group.web-modified
-
-  lifecycle {
-    destroy = false
-  }
+import {
+    to = aws_route_table.main
+    id = "rtb-02a0e7a85581495ff"
 }
+
+
+
+
